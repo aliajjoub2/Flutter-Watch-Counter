8b5748f4687bdb98bfd5a1c0b1a89a734466903f
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-08-01 15:20:04 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/countwatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.countwatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CounterWatch(),
+    );
+  }
+}
+
+class CounterWatch extends StatefulWidget {
+  const CounterWatch({Key? key}) : super(key: key);
+
+  @override
+  State<CounterWatch> createState() => _CounterWatchState();
+}
+
+class _CounterWatchState extends State<CounterWatch> {
+  Duration duration = const Duration(seconds: 0);
+
+  Timer? repeatedFunction;
+
+  bool isRunning = false;
+
+  startTimer() {
+    repeatedFunction = Timer.periodic(const Duration(seconds: 1), (qqqqqq) {
+      setState(() {
+        int neuSecond = duration.inSeconds + 1;
+        duration = Duration(seconds: neuSecond);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Watch Counter App'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 109, 186, 245),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(11),
+                  margin: const EdgeInsets.all(11),
+                  child: Text(duration.inHours.toString().padLeft(2, '0')),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(11),
+                  margin: const EdgeInsets.all(11),
+                  child: Text(duration.inMinutes
+                      .remainder(60)
+                      .toString()
+                      .padLeft(2, '0')),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(11),
+                  margin: const EdgeInsets.all(11),
+                  child: Text(duration.inSeconds
+                      .remainder(60)
+                      .toString()
+                      .padLeft(2, '0')),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                isRunning
+                    ? Row(
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                if (repeatedFunction!.isActive) {
+                                  setState(() {
+                                    repeatedFunction!.cancel();
+                                  });
+                                } else {
+                                  startTimer();
+                                }
+                              },
+                              child: Text(repeatedFunction!.isActive
+                                  ? 'Stop'
+                                  : 'Resume')),
+                          ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  duration = const Duration(seconds: 0);
+                                  isRunning = false;
+                                });
+                              },
+                              child: const Text('Cancel')),
+                        ],
+                      )
+                    : ElevatedButton(
+                        onPressed: () {
+                          startTimer();
+                          setState(() {
+                            isRunning = true;
+                          });
+                        },
+                        child: const Text('Start'))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
